36f6e0ed44b2764c92fa51e3ad24714f2b1416e2
brice-morin <brice.morin1@gmail.com>
2014-06-04 11:12:42 +0200

    fix test

--- src/test/java/org/thingml/java/Sample1Test.java
+++ src/test/java/org/thingml/java/Sample1Test.java
@@ -48,7 +48,7 @@ public class Sample1Test extends TestCase {
         }
 
         @Override
-        protected Component buildBehavior() {
+        public Component buildBehavior() {
             //Event types
             final NullEventType nullEventType = new NullEventType();
 
